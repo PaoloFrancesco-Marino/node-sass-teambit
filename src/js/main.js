cc61d95089654e956748c5de7c0f719b2a491493
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-05-17 11:01:38 +0200

    add dropmenu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
-console.log('js work');
+import $ from 'jquery';
+
+$(document).ready(function () {
+    
+    // refs
+    var navbarMenu = $('.navbar_menu .drop_click');
+    var dropClick = navbarMenu.children('a'); // a click
+    var dropMenu = navbarMenu.children('.dropdowm_menu'); // drop menu container
+
+
+    // event click
+    dropClick.click(function(){
+
+        var actualMenu = $(this).next('.dropdown_menu');
+
+        dropMenu.not(actualMenu).hide();
+
+        actualMenu.toggle();
+
+
+    });
+
+    // mouse enter 
+    navbarMenu.mouseenter(function () { 
+        $(this).children('.dropdown_menu').show();
+       
+    });
+
+    navbarMenu.mouseleave(function () { 
+        $(this).children('.dropdown_menu').hide();
+    });
+
+
+});
